3a0db1d5d2721e29edcae97f8960734f26e7728b
nabeelamir-defra <nabeel.amir@defra.gov.uk>
2020-12-11 15:00:15 +0000

    fixed issue with config getting deleted

--- src/lib/antivirus.js
+++ src/lib/antivirus.js
@@ -4,6 +4,7 @@ const NodeClam = require('clamscan')
 const wait = interval => new Promise(resolve => setTimeout(resolve, interval))
 
 async function retryAntiVirusInit (config, retries, delay) {
+  const configCopy = { ...config } // create copy of config as nodeclam modifies it in init method
   try {
     return await new NodeClam().init(config)
   } catch (err) {
@@ -13,7 +14,7 @@ async function retryAntiVirusInit (config, retries, delay) {
     logger.info(`Unable to find virus scanner - retries left ${retries}`)
     await wait(delay)
     const retry = retries - 1
-    return retryAntiVirusInit(config, retry, delay)
+    return retryAntiVirusInit(configCopy, retry, delay)
   }
 }
 
