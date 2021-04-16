0e125948835fa5d2aec2e4b45a7fd77db36ebeb4
nabeelamir-defra <nabeel.amir@defra.gov.uk>
2021-04-16 14:11:25 +0100

    added functionality to view submission

--- src/handlers/records-search-results.js
+++ src/handlers/records-search-results.js
@@ -21,17 +21,26 @@ module.exports = class RecordsSearchResultsHandler extends BaseHandler {
    */
   async doGet (request, h) {
     const cache = await request.cache().get()
-    if (!cache.contactId) {
+    if (!cache.recordsContactId) {
       return h.redirect('/records')
     }
-    const submissions = await submissionsApi.getByContactId(request, cache.contactId)
-
-    delete cache.contactId
-    await request.cache().set(cache)
+    const submissions = await submissionsApi.getByContactId(request, cache.recordsContactId)
 
     return h.view(this.path, {
       submissions,
       fullName: cache.fullName
     })
+  }
+
+  async doPost (request, h) {
+    const submission = await submissionsApi.getById(request, request.payload.submissionId)
+
+    const cache = await request.cache().get()
+    cache.recordsYear = submission.season
+    cache.recordsSubmissionId = request.payload.submissionId
+
+    await request.cache().set(cache)
+
+    return h.redirect('/records-submissions')
   }
 }
