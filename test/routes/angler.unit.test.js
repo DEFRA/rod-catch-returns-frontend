d2d51e5495d63a3606a0a606c3dc44bc0006f121
Nabeel Amir <nabeel.amir@defra.gov.uk>
2025-05-14 14:45:15 +0100

    added tests

--- test/routes/angler.unit.test.js
+++ test/routes/angler.unit.test.js
@@ -16,7 +16,7 @@ describe('angler', () => {
   describe('/', () => {
     const rootRoute = angler.find(route => route.path === '/')
 
-    it('should redirect to /licence-auth if it is the public site', () => {
+    it('should redirect / to /licence-auth if it is the public site', () => {
       process.env.CONTEXT = 'ANGLER'
       const h = getMockH()
 
@@ -25,7 +25,7 @@ describe('angler', () => {
       expect(h.redirect).toHaveBeenCalledWith('/licence-auth')
     })
 
-    it('should redirect to /licence if it is the admin site', () => {
+    it('should redirect / to /licence if it is the admin site', () => {
       process.env.CONTEXT = 'FMT'
       const h = getMockH()
       rootRoute.handler({}, h)
