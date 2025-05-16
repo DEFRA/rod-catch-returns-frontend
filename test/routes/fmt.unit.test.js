7d062530384c9dfb3dbf0f3c553a958fbc41abd9
Nabeel Amir <nabeel.amir@defra.gov.uk>
2025-05-16 14:37:28 +0100

    added more unit tests

--- test/routes/fmt.unit.test.js
+++ test/routes/fmt.unit.test.js
@@ -1,4 +1,5 @@
 const fmt = require('../../src/routes/fmt')
+const { getMockH } = require('../test-utils/server-test-utils')
 
 describe('fmt', () => {
   describe('/login', () => {
@@ -28,28 +29,44 @@ describe('fmt', () => {
   })
 
   describe('/oidc/account-disabled', () => {
-    const oidcSignIn = fmt.find(route => route.path === '/oidc/account-disabled')
+    const accounDisabled = fmt.find(route => route.path === '/oidc/account-disabled')
 
     it('should have a /oidc/account-disabled route with auth as false', () => {
-      expect(oidcSignIn.options).toStrictEqual({
+      expect(accounDisabled.options).toStrictEqual({
         auth: false,
         plugins: {
           crumb: false
         }
       })
     })
+
+    it('should have a handler that shows the account disabled screen', () => {
+      const h = getMockH()
+
+      accounDisabled.handler({}, h)
+
+      expect(h.view).toHaveBeenCalledWith('account-disabled')
+    })
   })
 
   describe('/oidc/account-role-required', () => {
-    const oidcSignIn = fmt.find(route => route.path === '/oidc/account-role-required')
+    const accountRoleRequired = fmt.find(route => route.path === '/oidc/account-role-required')
 
     it('should have a /oidc/account-role-required route with auth as false', () => {
-      expect(oidcSignIn.options).toStrictEqual({
+      expect(accountRoleRequired.options).toStrictEqual({
         auth: false,
         plugins: {
           crumb: false
         }
       })
+    })
+
+    it('should have a handler that shows the role required screen', () => {
+      const h = getMockH()
+
+      accountRoleRequired.handler({}, h)
+
+      expect(h.view).toHaveBeenCalledWith('account-role-required')
     })
   })
 })
