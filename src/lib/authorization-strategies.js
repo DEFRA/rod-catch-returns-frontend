eae410f7bf712fcdf78988d99e3076f45dd597bc
Nabeel Amir <nabeel.amir@defra.gov.uk>
2025-06-20 10:58:15 +0100

    added additional unit test

--- src/lib/authorization-strategies.js
+++ src/lib/authorization-strategies.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const { validateSession } = require('./session-validator')
+
 /**
  * This file contains the handlers for the authorization strategies used by hapi
  */
@@ -13,24 +15,7 @@ module.exports = {
     },
 
     redirectTo: '/licence-auth',
-    /**
-     * validation function called on every request
-     * When the cache-entry expires the user has to re-authenticate
-     */
-    validateFunc: async (request, session) => {
-      const server = request.server
-      const cached = await server.app.cache.get(session.sid)
-
-      const out = {
-        valid: !!cached
-      }
-
-      if (out.valid) {
-        out.credentials = cached.authorization
-      }
-
-      return out
-    }
+    validateFunc: validateSession
   },
   adminCookie: {
     cookie: {
@@ -44,23 +29,6 @@ module.exports = {
     },
     redirectTo: '/login',
     appendNext: true,
-    /**
-     * validation function called on every request
-     * When the cache-entry expires the user has to re-authenticate
-     */
-    validateFunc: async (request, session) => {
-      const server = request.server
-      const cached = await server.app.cache.get(session.sid)
-
-      const out = {
-        valid: !!cached
-      }
-
-      if (out.valid) {
-        out.credentials = cached.authorization
-      }
-
-      return out
-    }
+    validateFunc: validateSession
   }
 }
