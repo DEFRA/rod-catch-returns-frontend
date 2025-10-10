4d1d46290db6e4a49a85955039c411d05822da3b
Nabeel Amir <nabeel.amir@defra.gov.uk>
2025-10-10 15:42:33 +0100

    refactored test

--- test/validators/licence.unit.test.js
+++ test/validators/licence.unit.test.js
@@ -1,4 +1,5 @@
 const licenceValidator = require('../../src/validators/licence')
+const ResponseError = require('../../src/handlers/response-error')
 const { parsePostcode, parseLicence, licenceSchema } = require('../../src/lib/licence-utils')
 const LicenceApi = require('../../src/api/licence')
 const { logger } = require('defra-logging-facade')
@@ -6,6 +7,30 @@ const { logger } = require('defra-logging-facade')
 jest.mock('../../src/api/licence')
 jest.mock('../../src/lib/licence-utils')
 jest.mock('defra-logging-facade')
+
+const getMockRequest = ({ postcode = 'A9 9AA', licence = '123456' } = {}) => ({
+  payload: {
+    licence,
+    postcode
+  },
+  app: {}
+})
+
+const setupMocks = ({
+  postcode = 'A9 9AA',
+  licence = '123456',
+  licenceApiResponse = () => ({
+    contact: {
+      id: '12345'
+    }
+  }),
+  licenceSchemaResponse = { value: {} }
+} = {}) => {
+  parsePostcode.mockReturnValueOnce(postcode)
+  parseLicence.mockReturnValueOnce(licence)
+  LicenceApi.getContactFromLicenceKey.mockImplementation(licenceApiResponse)
+  licenceSchema.validate.mockReturnValueOnce(licenceSchemaResponse)
+}
 
 describe('licence.unit', () => {
   beforeEach(() => {
@@ -36,25 +61,17 @@ describe('licence.unit', () => {
 
   it('should return VALIDATION_ERROR if there is an error with the validation', async () => {
     licenceSchema.validate.mockReturnValueOnce({ error: 'error' })
-    const request = {
-      payload: {
-        licence: '123456',
-        postcode: 'A9 9AA'
-      }
-    }
-    const result = await licenceValidator(request)
+    const result = await licenceValidator(getMockRequest())
 
     expect(result).toStrictEqual([{ licence: 'VALIDATION_ERROR' }])
   })
 
   it('should return NOT_FOUND if no contact is returned', async () => {
-    parseLicence.mockReturnValueOnce('123456')
-    parsePostcode.mockReturnValueOnce('A9 9AA')
-    licenceSchema.validate.mockReturnValueOnce({ value: {} })
-    LicenceApi.getContactFromLicenceKey.mockResolvedValueOnce(null)
+    setupMocks({
+      licenceApiResponse: null
+    })
 
-    const request = { payload: { licence: '123456', postcode: 'A9 9AA' } }
-    const result = await licenceValidator(request)
+    const result = await licenceValidator(getMockRequest())
 
     expect(result).toStrictEqual([{ licence: 'NOT_FOUND' }])
   })
@@ -63,43 +80,34 @@ describe('licence.unit', () => {
     ['NOT_FOUND', 404],
     ['FORBIDDEN', 403]
   ])('should return NOT_FOUND if API throws %s', async (_, statusCode) => {
-    parseLicence.mockReturnValueOnce('123456')
-    parsePostcode.mockReturnValueOnce('A9 9AA')
-    licenceSchema.validate.mockReturnValueOnce({ value: {} })
+    setupMocks({
+      licenceApiResponse: () => {
+        throw new ResponseError.Error('Error', statusCode)
+      }
+    })
 
-    const error = new Error('API error')
-    error.statusCode = statusCode
-    LicenceApi.getContactFromLicenceKey.mockRejectedValueOnce(error)
-
-    const request = { payload: { licence: '123456', postcode: 'A9 9AA' } }
-    const result = await licenceValidator(request)
+    const result = await licenceValidator(getMockRequest())
 
     expect(result).toStrictEqual([{ licence: 'NOT_FOUND' }])
   })
 
   it('should log and return null if API throws any other error', async () => {
-    parseLicence.mockReturnValueOnce('123456')
-    parsePostcode.mockReturnValueOnce('A9 9AA')
-    licenceSchema.validate.mockReturnValueOnce({ value: {} })
+    setupMocks({
+      licenceApiResponse: () => {
+        throw new ResponseError.Error('Error', 500)
+      }
+    })
 
-    const error = new Error('API error')
-    error.statusCode = 500
-    LicenceApi.getContactFromLicenceKey.mockRejectedValueOnce(error)
-
-    const request = { payload: { licence: '123456', postcode: 'A9 9AA' } }
-    const result = await licenceValidator(request)
+    const result = await licenceValidator(getMockRequest())
 
     expect(result).toBe(null)
     expect(logger.error).toHaveBeenCalledTimes(1)
   })
 
   it('should return null if validation and API call succeed', async () => {
-    parseLicence.mockReturnValueOnce('123456')
-    parsePostcode.mockReturnValueOnce('A9 9AA')
-    licenceSchema.validate.mockReturnValueOnce({ value: {} })
-    LicenceApi.getContactFromLicenceKey.mockResolvedValueOnce({ id: 'abc123' })
+    setupMocks()
 
-    const request = { payload: { licence: '123456', postcode: 'A9 9AA' } }
+    const request = getMockRequest()
     const result = await licenceValidator(request)
 
     expect(result).toBeNull()
