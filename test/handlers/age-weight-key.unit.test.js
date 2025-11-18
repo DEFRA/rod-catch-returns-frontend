12c90e560d9b0cf9ac046eafb4cf61700994c0a8
Nabeel Amir <nabeel.amir@defra.gov.uk>
2025-11-18 14:41:59 +0000

    added more tests

--- test/handlers/age-weight-key.unit.test.js
+++ test/handlers/age-weight-key.unit.test.js
@@ -108,5 +108,125 @@ describe('age-weight-key.unit', () => {
         )
       })
     })
+
+    describe('doPost', () => {
+      it('should redirect to age-weight-key-ok page when there are no errors', async () => {
+        setupGates()
+        const h = getMockH()
+        const request = getMockRequest({
+          payload: {
+            year: '2025',
+            gate: 'g1',
+            upload: { path: '/tmp/file.tmp', filename: 'file.csv' }
+          }
+        })
+        const handler = new AgeWeightKeyHandler('age-weight-key')
+
+        await handler.doPost(request, h, null)
+
+        expect(h.redirect).toHaveBeenCalledWith('/age-weight-key-ok')
+      })
+
+      it('should redirect to age-weight-key-conflict-check check when errors include OVERWRITE_DISALLOWED', async () => {
+        setupGates()
+        const request = getMockRequest({
+          payload: { year: '2025', gate: 'g1' }
+        })
+        const errors = [{ type: 'OVERWRITE_DISALLOWED' }]
+        const handler = new AgeWeightKeyHandler('age-weight-key')
+        const h = getMockH()
+
+        await handler.doPost(request, h, errors)
+
+        expect(h.redirect).toHaveBeenCalledWith('/age-weight-key-conflict-check')
+      })
+
+      it('should redirect back to upload page when errors exist but not OVERWRITE_DISALLOWED', async () => {
+        const mockSuperWriteCacheAndRedirect = BaseHandler.prototype.writeCacheAndRedirect = jest.fn()
+        setupGates()
+        const request = getMockRequest({
+          payload: {
+            year: '2025',
+            gate: 'g1',
+            upload: { path: '/tmp/file.tmp', filename: 'file.csv' }
+          }
+        })
+        const errors = [{ type: 'OTHER_ERROR' }]
+        const handler = new AgeWeightKeyHandler('age-weight-key')
+        const h = getMockH()
+
+        await handler.doPost(request, h, errors)
+
+        expect(mockSuperWriteCacheAndRedirect).toHaveBeenCalledWith(
+          request,
+          h,
+          errors,
+          null,
+          '/age-weight-key'
+        )
+      })
+
+      it('should populate cacheContext with gate info and upload details', async () => {
+        const mockSuperSetCacheContext = BaseHandler.prototype.setCacheContext = jest.fn()
+        setupGates()
+        const request = getMockRequest({
+          payload: {
+            year: '2025',
+            gate: 'g1',
+            upload: { path: '/tmp/file.tmp', filename: 'file.csv' }
+          }
+        })
+        const handler = new AgeWeightKeyHandler('age-weight-key')
+        const h = getMockH()
+
+        await handler.doPost(request, h, null)
+
+        expect(mockSuperSetCacheContext).toHaveBeenCalledWith(
+          request,
+          expect.objectContaining({
+            ageWeightKey: expect.objectContaining({
+              year: '2025',
+              gateName: 'Gate 1',
+              gateId: 'g1',
+              tempfile: '/tmp/file.tmp',
+              filename: 'file.csv'
+            }),
+            payload: request.payload,
+            errors: null
+          })
+        )
+      })
+
+      it('should populate cacheContext with an empty gate name and id, if they are not present', async () => {
+        const mockSuperSetCacheContext = BaseHandler.prototype.setCacheContext = jest.fn()
+        setupGates([])
+        const request = getMockRequest({
+          payload: {
+            year: '2025',
+            gate: 'g1',
+            upload: { path: '/tmp/file.tmp', filename: 'file.csv' }
+          }
+        })
+        const handler = new AgeWeightKeyHandler('age-weight-key')
+        const h = getMockH()
+
+        await handler.doPost(request, h, null)
+
+        expect(mockSuperSetCacheContext).toHaveBeenCalledWith(
+          request,
+          expect.objectContaining({
+            ageWeightKey: expect.objectContaining({
+              year: '2025',
+              gateName: '',
+              gateId: '',
+              tempfile: '/tmp/file.tmp',
+              filename: 'file.csv'
+            }),
+            payload: request.payload,
+            errors: null
+          })
+        )
+      })
+    })
   })
 })
