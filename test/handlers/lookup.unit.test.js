5b8ea191171bba2fae4288593061d395f1814568
Nabeel Amir <nabeel.amir@defra.gov.uk>
2025-12-01 11:33:34 +0000

    added tests

---
+++ test/handlers/lookup.unit.test.js
@@ -0,0 +1,95 @@
+const mockGetById = jest.fn()
+
+const LookupHandler = require('../../src/handlers/lookup')
+const { getMockH } = require('../test-utils/server-test-utils')
+
+jest.mock('../../src/api/submissions', () => {
+  return jest.fn().mockImplementation(() => ({
+    getById: mockGetById
+  }))
+})
+
+describe('save-handler.unit', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  const getMockRequest = (query = {}, cacheObj = {}) => ({
+    query,
+    cache: jest.fn(() => ({
+      get: jest.fn().mockResolvedValueOnce(cacheObj),
+      set: jest.fn().mockResolvedValueOnce()
+    }))
+  })
+
+  const getMockSubmission = (overrides) => ({
+    contactId: 'c1',
+    season: '2025',
+    ...overrides
+  })
+
+  describe('doGet', () => {
+    it('should throw ResponseError if submission not found', async () => {
+      const request = getMockRequest({ submissionId: 'submissions/1' })
+      const h = getMockH()
+      mockGetById.mockResolvedValueOnce(null)
+      const handler = new LookupHandler('lookup')
+
+      await expect(handler.doGet(request, h)).rejects.toThrow('Bad submission request')
+    })
+
+    it('should update cache and redirect to activityId if present', async () => {
+      const cacheObj = {}
+      const request = getMockRequest({ submissionId: 'submissions/1', activityId: '/activity/123' }, cacheObj)
+      const h = getMockH()
+      mockGetById.mockResolvedValueOnce(getMockSubmission())
+      const handler = new LookupHandler('lookup')
+
+      await handler.doGet(request, h)
+
+      expect(cacheObj).toEqual({
+        contactId: 'c1',
+        year: '2025',
+        submissionId: 'submissions/1',
+        back: '/summary'
+      })
+      expect(h.redirect).toHaveBeenCalledWith('/activity/123')
+    })
+
+    it('should redirect to smallCatchId if present', async () => {
+      const cacheObj = {}
+      const request = getMockRequest({ submissionId: 'submissions/1', smallCatchId: 'small-catches/2' }, cacheObj)
+      const h = getMockH()
+      mockGetById.mockResolvedValueOnce(getMockSubmission())
+      const handler = new LookupHandler('lookup')
+
+      await handler.doGet(request, h)
+
+      expect(h.redirect).toHaveBeenCalledWith('/small-catches/2')
+    })
+
+    it('should redirect to catchId if present', async () => {
+      const cacheObj = {}
+      const request = getMockRequest({ submissionId: 'submissions/1', catchId: 'catches/3' }, cacheObj)
+      const h = getMockH()
+      mockGetById.mockResolvedValueOnce(getMockSubmission())
+      const handler = new LookupHandler('lookup')
+
+      await handler.doGet(request, h)
+
+      expect(h.redirect).toHaveBeenCalledWith('/catches/3')
+    })
+
+    it('should redirect to summary if no specific id provided', async () => {
+      const cacheObj = {}
+      const request = getMockRequest({ submissionId: 'submissions/1' }, cacheObj)
+      const h = getMockH()
+      mockGetById.mockResolvedValueOnce(getMockSubmission())
+      const handler = new LookupHandler('lookup')
+
+      await handler.doGet(request, h)
+
+      expect(h.redirect).toHaveBeenCalledWith('/summary')
+    })
+  })
+})
